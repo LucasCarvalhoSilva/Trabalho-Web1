257ac9377c06a8d2ddec5b550c9d2ef9ce198619
Lucas Carvalho Silva <lucascarvalhosilva2901@gmail.com>
2022-11-08 20:14:16 -0300

    Inicio da segunda etapa do trabalho, adicião do auth do firebase e correção de bug no css da pagina home

---
+++ config/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBkZukvP9R-Q70lF7XNZUW0qDJi7HPkFXI",
+    authDomain: "myhealth-e5f2f.firebaseapp.com",
+    projectId: "myhealth-e5f2f",
+    storageBucket: "myhealth-e5f2f.appspot.com",
+    messagingSenderId: "684383200960",
+    appId: "1:684383200960:web:57e74f162c76f8c370ef25"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
